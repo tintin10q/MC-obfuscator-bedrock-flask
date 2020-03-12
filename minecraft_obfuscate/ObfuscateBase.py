d9a26aa597862ae4c01a02d4edb9a191167bd7ad
Quinten <tintin10q@hotmail.com>
2020-03-12 05:05:20 +0100

    Made it use the new blacklist format

--- minecraft_obfuscate/ObfuscateBase.py
+++ minecraft_obfuscate/ObfuscateBase.py
@@ -1,15 +1,14 @@
 import re
-from minecraft_obfuscate.IDgenerator import IDgenerator
-from database.database_manager import config
+from minecraft_obfuscate.IDgenerator import id_gen
 
-class ObfuscateBase(IDgenerator):
+class ObfuscateBase():
+
 
     def __init__(self, name: str, regex_patterns):
-        super().__init__()
         self.name = name
         self.len = len(self.name)
         self.regex_patterns = regex_patterns
-        self.new_name = self.get_new_random_name()
+        self.new_name = id_gen()
         self.key = (self.name, self.new_name)  # Used for the output key
         self.regex = [re.compile(r.format(name=self.name)) for r in regex_patterns]
 
@@ -23,7 +22,7 @@ class ObfuscateBase(IDgenerator):
         for r in self.regex:
             text = r.sub(r"\g<1>{}\g<3>".format(self.new_name), text)
         # Remove comments
-        if config["remove_comments"]:
+        if True:  #  TODO: config["remove_comments"]
             text = re.sub(re.compile("#.{0,}\n?"), "", text)
         # Remove blank lines
         blanklines_pat = re.compile("\n\n")
