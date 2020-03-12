17de1a8b74b245514990f0cad3378c5b04218977
Quinten <tintin10q@hotmail.com>
2020-03-12 05:02:26 +0100

    Made IDgenerator not use a database but a memory in a class. More like c# does it.

--- minecraft_obfuscate/IDgenerator.py
+++ minecraft_obfuscate/IDgenerator.py
@@ -1,44 +1,37 @@
 import random
-from database.database_manager import db
-from database.database_manager import config
 
-character_length = config["character_length"]
 
 class IDgenerator:
+    """An id generator that will never run out of ids to give and never give the same id not an actual generator :("""
 
-    obfuscate_characters = config["character_pool"]  # other options include O0, vw, mn,
+    def __init__(self, character_pool="O0", length=16):
+        self.character_pool = character_pool
+        self.length = length
+        self.used_UUIDs = []
+        self.max_ids = 2 ** length
 
-    def __call__(self, length=character_length):
-        return self.get_new_random_name(length)
+    def __call__(self):
+        return self.get_new_random_name()
 
-    @staticmethod
-    def generate_random_name(lenght=character_length):
+    def generate_random_name(self):
         uuid = ""
-        for i in range(0, lenght):
-            uuid += random.choice(IDgenerator.obfuscate_characters)
+        for i in range(self.length):
+            uuid += random.choice(self.character_pool)
         return uuid
 
-    @staticmethod
-    def get_new_random_name(lenght=character_length):
-        uuid = ""
-        used_uuids = db.get("used_uuids")
-        while uuid in used_uuids or uuid == "":
-            uuid = IDgenerator.generate_random_name(lenght)
-        db.insert("used_uuids", uuid)
+    def get_new_random_name(self):
+        uuid = None
+        while uuid in self.used_UUIDs or uuid is None:
+            uuid = self.generate_random_name()
+        self.used_UUIDs.append(uuid)
+        if len(self.used_UUIDs) == self.max_ids:  # Make the ids longer this way the generator will never run out
+            self.used_UUIDs.clear()
+            self.length += 1
+            self.max_ids = 2 ** self.length
         return uuid
 
-
-def generate_random_name(lenght=character_length):
-    uuid = ""
-    for i in range(0, lenght):
-        uuid += random.choice(IDgenerator.obfuscate_characters)
-    return uuid
+    def reset(self, character_pool="O0", length=16):
+        self.__init__(character_pool, length)
 
 
-def get_new_random_name(lenght=character_length):
-    uuid = ""
-    used_uuids = db.get("used_uuids")
-    while uuid in used_uuids or uuid == "":
-        uuid = generate_random_name(lenght)
-    db.insert("used_uuids", uuid)
-    return uuid
+id_gen = IDgenerator()
